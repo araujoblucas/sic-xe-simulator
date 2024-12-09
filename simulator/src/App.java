5371e9d107be51db630b1901ec7882313b981f1e
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-12-09 11:28:15 -0300

    Update

--- simulator/src/App.java
+++ simulator/src/App.java
@@ -1,5 +1,23 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carregar o arquivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("style.fxml"));
+
+        // Configurar a cena e o palco
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Aplicação JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
