94a63265f92741e1ba82dedbde5e4be71b0a44fc
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-12-19 11:51:12 -0300

    update

---
+++ simulator/src/Memoria.java
@@ -0,0 +1,10 @@
+public class Memoria {
+    
+    private static Integer[] memoria= new Integer[1000];
+
+    public Memoria(){
+        for(int i=0; i<1000; i++){
+            memoria[i]=0;
+        }
+    }
+}
