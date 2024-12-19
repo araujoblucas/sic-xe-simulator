51dafbcf1fc0a42472c669ef53b027408c82895d
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-12-19 13:24:23 -0300

    update

--- simulator/src/Memória/Memoria.java
+++ simulator/src/Memória/Memoria.java
@@ -4,6 +4,7 @@ public class Memoria {
     public Palavramem[] memoria;
 
     public Memoria(){
+        memoria= new Palavramem[1000];
         for(int i=0; i<1000; i++){
             memoria[i]= new Palavramem();
         }
