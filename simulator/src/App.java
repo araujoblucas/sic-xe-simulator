f8c185ecb0d7447e2d14551747c1d8b89fe3d979
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-12-19 16:27:35 -0300

    update

--- simulator/src/App.java
+++ simulator/src/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         memoria = new Memoria();
         registradores = new Registradores();
+        registradores.getRegistradores(1).setReg((byte) 0x01, (byte) 0x02, (byte) 0x03);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("style.fxml"));
         Parent root = loader.load();
--- simulator/src/Registradores/Registrador.java
+++ simulator/src/Registradores/Registrador.java
@@ -1,21 +1,27 @@
 package Registradores;
 
-
 public class Registrador {
     private byte[] reg;
 
-    public Registrador(){
-        reg=new byte[3];
+    public Registrador() {
+        reg = new byte[3];
     }
+
     public byte[] getReg() {
         return reg;
     }
-    
-    public String getValueAsString(){ 
-        StringBuilder hexValue=new StringBuilder();
-        for(byte b:reg){
-            hexValue.append(String.format("%02X", b)); 
-        } 
-        return hexValue.toString(); 
+
+    public void setReg(byte b1, byte b2, byte b3) {
+        reg[0] = b1;
+        reg[1] = b2;
+        reg[2] = b3;
+    }
+
+    public String getValueAsString() {
+        StringBuilder hexValue = new StringBuilder();
+        for (byte b : reg) {
+            hexValue.append(String.format("%02X", b));
+        }
+        return hexValue.toString();
     }
 }
