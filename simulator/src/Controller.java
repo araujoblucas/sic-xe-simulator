c258efd518703f72a81a5d36f2730860efa500f5
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-12-19 15:36:53 -0300

    update

--- simulator/src/Controller.java
+++ simulator/src/Controller.java
@@ -1,18 +1,9 @@
-import Registradores.Registrador;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
 public class Controller {
-    @FXML
-    private TableView<Registrador> TabelaReg;
 
-    public void initialize() {
-        ObservableList<Registrador> registradores = FXCollections.observableArrayList();
-        for (int i = 0; i < 8; i++) {
-            registradores.add(new Registrador());
-        }
-        TabelaReg.setItems(registradores); // Configura os itens da tabela
-    }
+    @FXML
+    private TableView<?> TabelaReg;
+
 }
